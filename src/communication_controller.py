c054df738541bb09e1f50f6b5b380b951dfdba68
ge23tam <nils.morbitzer@tum.de>
2022-02-11 19:43:02 +0100

    Update config and communication controller to address right endpoint

--- src/communication_controller.py
+++ src/communication_controller.py
@@ -11,11 +11,9 @@ class CommunicationController:
         
         # Store box_id for authentication
         with open("resources/config.json") as config_json:
-            self.box_id = json.load(config_json)["ID"]
-
-        # Store foward ngrok URLs
-        self.forward_url_port_8080 = "http://0574-2003-cd-df25-768d-9f37-1a79-2826-deb5.ngrok.io"
-        self.forward_url_port_8081 = "http://6cc0-2003-cd-df25-768d-9f37-1a79-2826-deb5.ngrok.io"
+            box_config = json.load(config_json)
+            self.box_id = box_config["ID"]
+            self.api_gateway_url = box_config["API_GATEWAY_URL"]
         
         # Use session so it is not necessary to rewrite cookies and JWT for every request
         self.session = requests.Session()
@@ -56,27 +54,8 @@ class CommunicationController:
 
     def authenticate(self, user_id : string) -> bool:
         # Perform basic authentication by using user_id and box_id
-        #return self._httpRequest('POST', "<URL>", self.params, self._getBaseHeaders, (user_id, self.box_id), True).status_code == 200
-        userRoleData = self.session.get(f"{self.forward_url_port_8081}/api/cas/users/{user_id}/role")
-        if userRoleData.status_code != 200:
-            return False
-        response = '[]'
-        if userRoleData.text == '"DELIVERER"':
-            response = self.session.put(f"{self.forward_url_port_8080}/api/ds/deliveries/deliverer/{user_id}/deposited/box/{self.box_id}")
-        elif userRoleData.text == '"CUSTOMER"':
-            response = self.session.put(f"{self.forward_url_port_8080}/api/ds/deliveries/user/{user_id}/delivered/box/{self.box_id}")
-        
+        response = self.session.put(f"{self.api_gateway_url}/api/ds/deliveries/user/{user_id}/update/box/{self.box_id}")
         return response.status_code == 200 and response.text != '[]'
-            
-    
-
-
-    """
-    def authenticate(self, user_id : string) -> bool:
-        with open("resources/user_ids.json") as user_ids_json:
-            user_ids = json.load(user_ids_json)
-            return {"id": user_id.strip()} in user_ids
-    """
 
 
     def sendNotificationToCustomer(self, id : string) -> bool:
